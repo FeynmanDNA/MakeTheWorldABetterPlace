34db0f58b48b835e492d49f250297391825757c6
Brahms <yangkaiyuan@u.nus.edu>
2018-07-06 20:01:30 +0800

    HTTPconfig.js NODE_ENV for build

--- src/HTTPconfig.js
+++ src/HTTPconfig.js
@@ -1,9 +1,17 @@
+// start script's process.env.NODE_ENV = 'development';
+// build script's process.env.NODE_ENV = 'production';
+// use different firebase database for different script
+// default as development
 const HTTPconfig = {
   // the client tells server data-type json is actually sent.
   HTTP_HEADER: {
     "Content-Type": "application/json",
   },
   gateway: "http://localhost:7717/",
+}
+
+if (process.env.NODE_ENV === "production") {
+  HTTPconfig.gateway = "http://feynmanDNA.com:7717/";
 }
 
 export default HTTPconfig;
